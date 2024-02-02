c1a63319e02001d695bbf9147253d4ae25331557
badohoun <espoir.badohoun@gmail.com>
2024-02-02 10:07:04 +0100

    add doc string  for add_numbers function

---
+++ git_training.py
@@ -0,0 +1,12 @@
+def add_numbers(a,b,c):
+    """return sum of a,b,c"""
+    return a + b + c
+
+#1. étendre add_numbers pour autoriser un nombre arbitraire de paramètres
+#2. investiguer et résoudre le problème de sécurité de add_numbers()
+#3. rendre l'implémentation de add_numbers() plus performante
+#4. ajouter de la documentation pour add_numbers()
+
+
+def calc_difference(a,b):
+    return a-b 
